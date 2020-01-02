ba623035a9153f0f585b79306ede4c534f3ee4dc
mzuckerb <50806077+mzuckerb@users.noreply.github.com>
2020-01-02 11:53:55 +0200

    tests

--- Solution1/PL/Program.cs
+++ Solution1/PL/Program.cs
@@ -9,15 +9,32 @@ namespace PL
         static void Main(string[] args)
         {
             IBL bl = FactoryBL.getIBL();
+            #region gs
             GuestRequest guesty = new GuestRequest();
+            GuestRequest guesty1 = new GuestRequest();
+            GuestRequest guesty2 = new GuestRequest();
+
             guesty.EntryDate1 = new DateTime(2020, 7, 7);
             guesty.ReleaseDate1 = new DateTime(2020, 7, 15);
             guesty.PrivateName1 = "moshe";
             guesty.Children1 = 5;
 
+            guesty1.EntryDate1 = new DateTime(2020, 5, 7);
+            guesty1.ReleaseDate1 = new DateTime(2020, 7, 23);
+            guesty1.PrivateName1 = "binyamin";
+            guesty1.Children1 = 5;
+
+            guesty2.EntryDate1 = new DateTime(2020, 5, 7);
+            guesty2.ReleaseDate1 = new DateTime(2020, 6, 15);
+            guesty2.PrivateName1 = "dovid";
+            guesty2.Children1 = 5;
+
             try
             {
                 bl.AddGuestRequest(guesty);
+                bl.AddGuestRequest(guesty1);
+                bl.AddGuestRequest(guesty2);
+
             }
             catch (Exception e)
             {
@@ -27,18 +44,18 @@ namespace PL
             guesty.Children1 = 3;
             guesty.PrivateName1 = "notmoshe";
 
-            /*try
+            try
              {
                  bl.UpdateGuestRequest(guesty);
              }
              catch (Exception e)
              {
                  Console.WriteLine(e);
-             }*/
+             }
 
-            try
+           try
             {
-                bl.DeleteGuestRequest(guesty);
+                bl.DeleteGuestRequest(guesty1);
             }
             catch (Exception e)
             {
@@ -49,6 +66,7 @@ namespace PL
             {
                 Console.WriteLine(i);
             }
+            #endregion
 
             Console.ReadKey();
         }
