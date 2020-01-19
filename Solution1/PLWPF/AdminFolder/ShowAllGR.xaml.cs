5c4b3279d685c495efe6acc94434d909af11eca0
binymarcus <48927315+binymarcus@users.noreply.github.com>
2020-01-19 13:02:05 +0200

    uiyy

---
+++ Solution1/PLWPF/AdminFolder/ShowAllGR.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PLWPF.AdminFolder
+{
+    /// <summary>
+    /// Interaction logic for ShowAllGR.xaml
+    /// </summary>
+    public partial class ShowAllGR : Window
+    {
+        public ShowAllGR()
+        {
+            InitializeComponent();
+        }
+    }
+}
